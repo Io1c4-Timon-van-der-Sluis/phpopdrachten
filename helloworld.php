d366b7a94a12487f63c644d22322155311c7ce76
Timon van der Sluis <timon.vandersluis@edu-kw1c.nl>
2020-02-05 15:10:44 +0100

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<html lang="en">
+    <head>
+        <meta charset="utf8" />
+        <title><?php print "pagina 1"; ?></title>
+    </head>
+    <body>
+    <h1>
+        <?php
+            echo "hallo wereld 1";
+
+        ?>
+    </h1>
+    </body>
+</html>
